acb558668f4d3b64773242fe77c0ac7ed55df73a
Rafael-Ramosrc <rafael.ramosrc@gmail.com>
2024-02-01 09:45:04 -0300

    reafctor: add new message container style

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,17 @@ import NoteModal from "./components/note.modal";
 import CreateNote from "./components/notes/create.note";
 import NoteItem from "./components/notes/note.component";
 import NProgress from "nprogress";
+import { max } from "date-fns";
+
+const styles = {
+
+  mensagem_container: {
+    maxwidth: '68rem',
+    margin: 'auto',
+  }
+
+};
+
 
 function AppContent() {
   const [openNoteModal, setOpenNoteModal] = useState(false);
@@ -49,7 +60,7 @@ function AppContent() {
   }, [isLoading, isFetching]);
 
   return (
-    <div className="2xl:max-w-[90rem] max-w-[68rem] mx-auto">
+    <div style={styles.mensagem_container}>
       <div className="m-8 grid grid-cols-[repeat(auto-fill,_320px)] gap-7 grid-rows-[1fr]">
         <div className="p-4 min-h-[18rem] bg-white rounded-lg border border-gray-200 shadow-md flex flex-col items-center justify-center">
           <div
@@ -62,7 +73,7 @@ function AppContent() {
             onClick={() => setOpenNoteModal(true)}
             className="text-lg font-medium text-ct-blue-600 mt-5 cursor-pointer"
           >
-            Add new note
+            Deixa uma mensagem para meu servidor!
           </h4>
         </div>
         {/* Note Items */}
